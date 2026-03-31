8a0669490a439f891d3f7e9593f8052e2f7a2418
DiegoReyPikzo <20253rd033@utez.edu.mx>
2026-03-31 14:32:16 -0600

    cree la función de onRetiro y agregué el gif para el menusito

--- src/main/java/com/example/sisbancario/HelloOpciones.java
+++ src/main/java/com/example/sisbancario/HelloOpciones.java
@@ -1,4 +1,27 @@
 package com.example.sisbancario;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView; //importé todas las dependencias  :D e incluso agregué una que me ayuda a poner un gifsito
+import javafx.stage.Stage;
 public class HelloOpciones {
+    @FXML
+    private  WebView gifRupia;
+    @FXML
+    private  void initialize(){
+        gifRupia.getEngine().load(getClass().getResource("GifRupi.gif").toExternalForm());
+    }
+
+    @FXML
+    protected void onRetiro(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Retiro.view.fxml"));
+            Stage stage = (Stage) gifRupia.getScene().getWindow();
+            stage.setScene(new Scene(loader.load()));
+            stage.show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module com.example.sisbancario {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.example.sisbancario to javafx.fxml;
+    opens com.example.sisbancario to javafx.fxml, javafx.web;
     exports com.example.sisbancario;
 }
