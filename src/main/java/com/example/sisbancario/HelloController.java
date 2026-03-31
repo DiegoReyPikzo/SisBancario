4ad2fdc169c29888785df5d4126ebbd0d712d887
DiegoReyPikzo <20253rd033@utez.edu.mx>
2026-03-31 03:22:30 -0600

    imTerminé ventana principal o eso creo :DDD

--- src/main/java/com/example/sisbancario/HelloController.java
+++ src/main/java/com/example/sisbancario/HelloController.java
@@ -7,14 +7,38 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField usuario;
+    @FXML
+    private TextField contrasena;  //Id de los campos y texto
+    @FXML
+    private Label mensajeError;
+
+    @FXML
+    protected void onSesion() throws IOException {
+        String user = usuario.getText();
+        String pass = contrasena.getText();
+
+        if (user.equals("admin") && pass.equals("admin1234")){
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Opcionsotas"));
+                Stage stage = (Stage) usuario.getScene().getWindow();
+                stage.setScene(new Scene(loader.load()));
+                stage.show();
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }else {
+            mensajeError.setText("Contraseña o ussuario incorrectos");
+        }
     }
     // Oal amigazos
 }
