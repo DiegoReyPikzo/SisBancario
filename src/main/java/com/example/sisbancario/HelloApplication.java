ad96074db1e74e8598d99167e717724d2ff0c41a
DiegoReyPikzo <20253rd033@utez.edu.mx>
2026-04-01 01:38:09 -0600

    TerminÂAgregué un comentario vitalicio meri haz casso jaja like

--- src/main/java/com/example/sisbancario/HelloApplication.java
+++ src/main/java/com/example/sisbancario/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Banco Bañorte!");
-        stage.setScene(scene);
+        stage.setScene(scene); // Decoralo en fa Meri gei porfa
         stage.show();
     }
 }
