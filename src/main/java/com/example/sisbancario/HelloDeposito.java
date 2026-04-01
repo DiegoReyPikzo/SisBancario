f82787e70ce9d470759f6d3e883533c3ec64c9df
Merytrubu <20253rd036@utez.edu.mx>
2026-03-31 22:31:35 -0600

    Modificaciones minimas (solo para cargar la actualización)

--- src/main/java/com/example/sisbancario/HelloDeposito.java
+++ src/main/java/com/example/sisbancario/HelloDeposito.java
@@ -1,4 +1,34 @@
 package com.example.sisbancario;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent; //Importando las librerias ;DDDDD
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class HelloDeposito {
+    @FXML
+    private TextField txtCanti;
+    @FXML
+    private Button btonVolver;
+    @FXML
+    private  void confiRetiro(){
+        String cantidad = txtCanti.getText();
+        System.out.println("Depositando: $" + cantidad);
+
+    }
+    @FXML
+    protected void volverMenu() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("opciones-view.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) btonVolver.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
 }
