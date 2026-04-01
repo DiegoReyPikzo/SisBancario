ce0fd2879ec56d66965ea6e9eefc242edd83f8e8
DiegoReyPikzo <20253rd033@utez.edu.mx>
2026-03-31 18:16:54 -0600

    Terminé lÃa configuración básica de el hello retiro

--- src/main/java/com/example/sisbancario/HelloRetiro.java
+++ src/main/java/com/example/sisbancario/HelloRetiro.java
@@ -1,4 +1,33 @@
 package com.example.sisbancario;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent; //Importando las librrerias ;DDDDD
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 public class HelloRetiro {
+    @FXML
+    private TextField txtCanti;
+    @FXML
+    private Button btonVolver;
+    @FXML
+    private  void confiRetiro(){
+        String cantidad = txtCanti.getText();
+        System.out.println("Retirando: $" + cantidad);
+
+    }
+    @FXML
+    protected void volverMenu() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("opciones-view.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) btonVolver.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
 }
