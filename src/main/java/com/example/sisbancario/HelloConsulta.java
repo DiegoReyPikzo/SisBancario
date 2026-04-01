ed7a9bc0f56ba0a80aa593343fb368b0be65a6fe
David-Flores1998 <20253rd041@utez.edu.mx>
2026-04-01 00:30:37 -0600

    agregamos que se pueda mostrar la cantidad que se tiene en le banco para que aparezca en el textfield modificando un poco el consulta-view y eliminando comandos inecesarios

--- src/main/java/com/example/sisbancario/HelloConsulta.java
+++ src/main/java/com/example/sisbancario/HelloConsulta.java
@@ -1,8 +1,40 @@
 package com.example.sisbancario;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import java.io.IOException;
 public class HelloConsulta {
+    @FXML
+    private Button btonVolver3;
 
-}
+    @FXML
+    private TextField lblSaldo;
+
+    @FXML
+    private void consultarSaldo() {
+        lblSaldo.setText("$" + String.format("%.2f", Banco.saldo));
+    }
+
+    @FXML
+    private void volverMenu() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("opciones-view.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Menusito bonito");
+            stage.setScene(new Scene(root, 530, 500));
+            stage.show();
+            ((Stage) btonVolver3.getScene().getWindow()).close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+
+
+
